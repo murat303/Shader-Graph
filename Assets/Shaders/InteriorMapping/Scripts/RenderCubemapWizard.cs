f72d814f848819c875e46cb64fdbbe7dfc902a98
murat303 <murat303@gmail.com>
2020-08-10 00:57:19 +0300

    Interior Mapping

---
+++ Assets/Shaders/InteriorMapping/Scripts/RenderCubemapWizard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RenderCubemapWizard : ScriptableWizard
+{
+    public Camera camera;
+
+    private void OnWizardUpdate()
+    {
+        isValid = (camera != null);
+    }
+
+    private void OnWizardCreate()
+    {
+        Cubemap cubemap = new Cubemap(512, TextureFormat.ARGB32, false);
+        camera.RenderToCubemap(cubemap);
+        AssetDatabase.CreateAsset(cubemap, $"Assets/Shaders/InteriorMapping/Cubemaps/{camera.name}.cubemap");
+    }
+
+    [MenuItem("Tools/Cubemap Wizard")]
+    static void RenderCubemap()
+    {
+        DisplayWizard<RenderCubemapWizard>("Render Cuvemap", "Render");
+    }
+}
